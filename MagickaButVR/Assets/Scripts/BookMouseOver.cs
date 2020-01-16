a903d41950b23b275341029f3d9d303bae81a443
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2020-01-16 17:32:55 -0600

    tree in

--- MagickaButVR/Assets/Scripts/BookMouseOver.cs
+++ MagickaButVR/Assets/Scripts/BookMouseOver.cs
@@ -25,34 +25,44 @@ public class BookMouseOver : MonoBehaviour,IPointerEnterHandler,IPointerExitHand
         }
         if (this.GetComponent<Text>().text == "GreaseBall Q-R-E")
         {
+            SpellIcon.GetComponent<Image>().sprite = Resources.Load<Sprite>("Placeholder");
+            SpellTitle.GetComponent<Text>().text = "Grease Ball";
             SpellDesc.GetComponent<Text>().text = "Geeeease Bitch";
         }
         if (this.GetComponent<Text>().text == "Jump E-Q-Q")
         {
+            SpellIcon.GetComponent<Image>().sprite = Resources.Load<Sprite>("Placeholder");
+            SpellTitle.GetComponent<Text>().text = "Jump";
             SpellDesc.GetComponent<Text>().text = "BOING BOING";
         }
         if (this.GetComponent<Text>().text == "Haste E-Q-F")
         {
+            SpellIcon.GetComponent<Image>().sprite = Resources.Load<Sprite>("Placeholder");
+            SpellTitle.GetComponent<Text>().text = "Haste";
             SpellDesc.GetComponent<Text>().text = "nyoooooom";
         }
         if (this.GetComponent<Text>().text == "Levitation E-E-Q")
         {
+            SpellIcon.GetComponent<Image>().sprite = Resources.Load<Sprite>("Placeholder");
+            SpellTitle.GetComponent<Text>().text = "Levitation";
             SpellDesc.GetComponent<Text>().text = "Floaty Boi";
-        }
-        if (this.GetComponent<Text>().text == "Haste E-Q-F")
-        {
-            SpellDesc.GetComponent<Text>().text = "nyoooooom";
         }
         if (this.GetComponent<Text>().text == "Telekinesis R-E-Q")
         {
+            SpellIcon.GetComponent<Image>().sprite = Resources.Load<Sprite>("Placeholder");
+            SpellTitle.GetComponent<Text>().text = "Telekinesis";
             SpellDesc.GetComponent<Text>().text = "Who needs to lift stuff?";
         }
         if (this.GetComponent<Text>().text == "Light F-R-F")
         {
+            SpellIcon.GetComponent<Image>().sprite = Resources.Load<Sprite>("Placeholder");
+            SpellTitle.GetComponent<Text>().text = "Light";
             SpellDesc.GetComponent<Text>().text = "BRIGHT sparkly bitch";
         }
         if (this.GetComponent<Text>().text == "Identify E-R-F")
         {
+            SpellIcon.GetComponent<Image>().sprite = Resources.Load<Sprite>("Placeholder");
+            SpellTitle.GetComponent<Text>().text = "Identify";
             SpellDesc.GetComponent<Text>().text = "I Taste Colors";
         }
 		#endregion
