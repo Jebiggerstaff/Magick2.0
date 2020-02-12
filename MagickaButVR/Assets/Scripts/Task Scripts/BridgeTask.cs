a0db1bc786726c54086dc7e4d89d9353294ea980
Conner <clmeriweather@gmail.com>
2020-02-12 16:02:37 -0600

    Asset organization, couple of tasks

---
+++ MagickaButVR/Assets/Scripts/Task Scripts/BridgeTask.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeTask : MonoBehaviour
+{
+    void OnTriggerEnter(Collider obj)
+    {
+        GameObject tmp = obj.gameObject;
+        if(tmp.name == "Plank")
+        {
+            tmp.GetComponent<Rigidbody>().isKinematic = false;
+            PersistentManager.instance.repairBridge = true;
+            PersistentManager.instance.repairBridgeCB.SetActive(true);
+            PersistentManager.instance.TaskComplete(true, "Bridge to the forest has been repaired.");
+        }
+    }
+
+    void OnTriggerExit(Collider obj)
+    {
+        GameObject tmp = obj.gameObject;
+        if (tmp.name == "Plank")
+        {
+            tmp.GetComponent<Rigidbody>().isKinematic = true;
+            PersistentManager.instance.repairBridge = false;
+            PersistentManager.instance.repairBridgeCB.SetActive(false);
+        }
+    }
+}
