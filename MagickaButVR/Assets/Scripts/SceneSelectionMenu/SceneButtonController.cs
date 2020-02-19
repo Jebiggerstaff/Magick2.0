7ad2e01535cd0d609edb489fd2db983a6fbc0a98
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2020-02-19 11:25:16 -0600

    Scene Selection Screen
    Allows the player to pick between the VR Game mode and the Main Game mode.

---
+++ MagickaButVR/Assets/Scripts/SceneSelectionMenu/SceneButtonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
+using System;
+using UnityEngine.SceneManagement;
+
+public class SceneButtonController : MonoBehaviour
+{
+
+    Button VRGameButton;
+    Button MainGameButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRSettings.enabled = false;
+
+        VRGameButton.onClick.AddListener(VRGame);
+        MainGameButton.onClick.AddListener(MainGame);
+    }
+
+    void VRGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+    void MainGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
