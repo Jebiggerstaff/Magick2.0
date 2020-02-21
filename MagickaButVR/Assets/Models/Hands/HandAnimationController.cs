4b92740d6beb5d4a351dedf5c1b2909d6c1db227
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2020-02-20 18:25:52 -0600

    Cave work and Hands

---
+++ MagickaButVR/Assets/Models/Hands/HandAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimationController : MonoBehaviour
+{
+    public AnimationClip handClose;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("z"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("Playing", true);
+            Invoke("StopAnim",handClose.length);
+        }
+    }
+
+    void StopAnim()
+    {
+        gameObject.GetComponent<Animator>().SetBool("Playing", false);
+
+    }
+}
---
+++ MagickaButVR/Assets/Scripts/Ladder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+
+    public GameObject player;
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+
+        if (collision.gameObject == player)
+        {
+            player.GetComponent<FirstPersonAIO>().useHeadbob = false;
+            player.GetComponent<Transform>().position += new Vector3(0, .2f, 0);
+        }
+       
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        player.GetComponent<FirstPersonAIO>().useHeadbob = true;
+    }
+
+}
