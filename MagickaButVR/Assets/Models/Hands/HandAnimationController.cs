895019fcbc007672cead988a62c6d61c9cd43047
p.love <phyzon.milton@trojans.dsu.edu>
2020-02-25 12:07:30 -0600

    Updated Hand Animation Script

--- MagickaButVR/Assets/Models/Hands/HandAnimationController.cs
+++ MagickaButVR/Assets/Models/Hands/HandAnimationController.cs
@@ -8,16 +8,64 @@ public class HandAnimationController : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown("q"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("isEvocation", true);
+            Invoke("StopEvocationAnim",handClose.length);
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("isEnchantment", true);
+            Invoke("StopEnchantmentAnim",handClose.length);
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("isTransmutation", true);
+            Invoke("StopTransmutationAnim",handClose.length);
+        }
+
+        if (Input.GetKeyDown("f"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("isConjuration", true);
+            Invoke("StopConjurationAnim",handClose.length);
+        }
+
         if (Input.GetKeyDown("z"))
         {
-            gameObject.GetComponent<Animator>().SetBool("Playing", true);
-            Invoke("StopAnim",handClose.length);
+            gameObject.GetComponent<Animator>().SetBool("isHandClose", true);
+            Invoke("StopHandCloseAnim",handClose.length);
         }
     }
 
-    void StopAnim()
+    void StopEvocationAnim()
     {
-        gameObject.GetComponent<Animator>().SetBool("Playing", false);
+        gameObject.GetComponent<Animator>().SetBool("isEvocation", false);
+
+    }
+
+    void StopEnchantmentAnim()
+    {
+        gameObject.GetComponent<Animator>().SetBool("isEnchantment", false);
+
+    }
+
+    void StopTransmutationAnim()
+    {
+        gameObject.GetComponent<Animator>().SetBool("isTransmutation", false);
+
+    }
+
+    void StopConjurationAnim()
+    {
+        gameObject.GetComponent<Animator>().SetBool("isConjuration", false);
+
+    }
+
+    void StopHandCloseAnim()
+    {
+        gameObject.GetComponent<Animator>().SetBool("isHandClose", false);
 
     }
 }
