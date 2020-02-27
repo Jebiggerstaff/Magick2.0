2925ae900d2442789a6b4825df07d9113e0e58e1
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2020-02-27 17:51:12 -0600

    Wire and castle gate lever working

---
+++ MagickaButVR/Assets/Scripts/LeverController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject gate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            Destroy(gate);
+        }
+    }
+}
